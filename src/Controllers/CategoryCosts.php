de76437c88d229506ee863d0f6ed634efea55998
PauloMazzia <pmazzia@gmail.com>
2017-10-18 20:08:37 -0200

    CRUD Categoria Usuarios

--- src/Controllers/CategoryCosts.php
+++ src/Controllers/CategoryCosts.php
@@ -8,11 +8,12 @@ $app
     ->get('/category-costs', function () use ($app) {
         $view = $app->service('view.renderer');
         $repository = $app->service('category-costs.repository');
-        $categories = $repository->all();
+        $auth = $app->service('auth');
+        $categories = $repository->findByField('user_id', $auth->user()->getId());
         return $view->render('category-costs/list.html.twig',
-                [
-                        'categories' => $categories
-                ]);
+            [
+                'categories' => $categories
+            ]);
     }, 'category-costs.list')
 
     ->get('/category-costs/new', function () use ($app) {
@@ -23,43 +24,61 @@ $app
     ->post('/category-costs/store', function (ServerRequestInterface $request) use ($app) {
         $data = $request->getParsedBody();
         $repository = $app->service('category-costs.repository');
+        $auth = $app->service('auth');
+        $data['user_id'] = $auth->user()->getId();
         $repository->create($data);
         return $app->route('category-costs.list');
     }, 'category-costs.store')
 
     ->get('/category-costs/{id}/edit', function (ServerRequestInterface $request) use ($app) {
         $view = $app->service('view.renderer');
-        $repository = $app->service('category-costs.repository');
         $id = $request->getAttribute('id');
-        $category = $repository->find($id);
+        $repository = $app->service('category-costs.repository');
+        $auth = $app->service('auth');
+        $category = $repository->findOneBy([
+            'id' => $id,
+            'user_id' => $auth->user()->getId()
+        ]);
         return $view->render('category-costs/edit.html.twig',
-                [
-                        'category' => $category
-                ]);
+        [
+            'category' => $category
+        ]);
     }, 'category-costs.edit')
 
-    ->post('/category-costs/{id}/update', function (ServerRequestInterface $request) use ($app) {
+    ->post('/category-costs/{id}/update', function(ServerRequestInterface $request) use($app) {
         $repository = $app->service('category-costs.repository');
         $id = $request->getAttribute('id');
         $data = $request->getParsedBody();
-        $repository->update($id, $data);
+        $auth = $app->service('auth');
+        $data['user_id'] = $auth->user()->getId();
+        $repository->update([
+            'id' => $id,
+            'user_id' => $auth->user()->getId()
+        ], $data);
         return $app->route('category-costs.list');
     }, 'category-costs.update')
 
-    ->get('/category-costs/{id}/show', function (ServerRequestInterface $request) use ($app) {
+    ->get('/category-costs/{id}/show', function(ServerRequestInterface $request) use($app){
         $view = $app->service('view.renderer');
-        $repository = $app->service('category-costs.repository');
         $id = $request->getAttribute('id');
-        $category = $repository->find($id);
-        return $view->render('category-costs/show.html.twig',
-                [
-                        'category' => $category
-                ]);
+        $repository = $app->service('category-costs.repository');
+        $auth = $app->service('auth');
+        $category = $repository->findOneBy([
+            'id' => $id,
+            'user_id' => $auth->user()->getId()
+        ]);
+        return $view->render('category-costs/show.html.twig', [
+            'category' => $category
+        ]);
     }, 'category-costs.show')
 
-    ->get('/category-costs/{id}/delete', function (ServerRequestInterface $request) use ($app) {
+    ->get('/category-costs/{id}/delete', function(ServerRequestInterface $request) use($app){
         $repository = $app->service('category-costs.repository');
         $id = $request->getAttribute('id');
-        $repository->delete($id);
+        $auth = $app->service('auth');
+        $repository->delete([
+            'id' => $id,
+            'user_id' => $auth->user()->getId()
+        ]);
         return $app->route('category-costs.list');
-    }, 'category-costs.delete');
+    }, 'category-costs.delete');;
